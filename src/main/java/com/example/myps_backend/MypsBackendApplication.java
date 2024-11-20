90cc238c1f5b012a4dc07dadb924dc442dae0c97
uommou <ffffff.990917@gmail.com>
2024-11-21 05:28:07 +0900

    Initial Commit

---
+++ src/main/java/com/example/myps_backend/MypsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.myps_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypsBackendApplication.class, args);
+	}
+
+}
